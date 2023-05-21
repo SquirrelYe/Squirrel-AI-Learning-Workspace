d321bdbb7adb64db3fa2b23fe726f5e1ba387060
@willye <willye@tencent.com>
2023-05-21 20:16:09 +0800

    fix: 🐛 xxx

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -4,7 +4,7 @@
  * @time 2023.05.19 16:46:18
  */
 export class OpenAIConfiguration {
-  public static readonly OPENAI_API_KEY: string = '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';
-  public static readonly OPENAI_Organization_ID: string = 'org-RFW8km1miJDGMXotInhmCmsP';
-  public static readonly OPENAI_Organization_Name: string = 'SquirrelJS';
+  public static readonly OPENAI_API_KEY: string = '';
+  public static readonly OPENAI_Organization_ID: string = '';
+  public static readonly OPENAI_Organization_Name: string = '';
 }
