8d3de0be9d7d2af5b2d9ac45a764a9a0ba33f2d8
@willye <willye@tencent.com>
2023-08-22 10:53:56 +0800

    feat: 🎸 归档代码

---
+++ LangChain/Document/02-Data Connection/4.Vector stores/VectorStore.py
@@ -0,0 +1,25 @@
+from langchain.document_loaders import TextLoader, UnstructuredPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+baseDir = "./files/"
+
+# 加载PDF文档
+loader = UnstructuredPDFLoader(baseDir + "index.pdf")
+content = loader.load()
+
+# 将文档分割成块
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+documents = text_splitter.split_documents(content)
+db = Chroma.from_documents(documents, OpenAIEmbeddings())
+
+# 执行相似性搜索
+query = "What is LLMSingleActionAgent?"
+# docs = db.similarity_search(query)
+# print(docs[0].page_content)
+
+# 还可以搜索与给定嵌入向量类似的文档，使用similarity_search_by_vector该向量接受嵌入向量作为参数而不是字符串。
+embedding_vector = OpenAIEmbeddings().embed_query(query)
+docs = db.similarity_search_by_vector(embedding_vector)
+print(docs[0].page_content)
