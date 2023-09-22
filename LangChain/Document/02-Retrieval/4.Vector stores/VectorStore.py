9dfa442694a3d5c276df0027a16a1cf80578b01c
@willye <willye@tencent.com>
2023-09-22 11:06:42 +0800

    docs: ✏️ 归档代码

--- LangChain/Document/02-Retrieval/4.Vector stores/VectorStore.py
+++ LangChain/Document/02-Retrieval/4.Vector stores/VectorStore.py
@@ -10,16 +10,17 @@ loader = UnstructuredPDFLoader(baseDir + "index.pdf")
 content = loader.load()
 
 # 将文档分割成块
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0)
 documents = text_splitter.split_documents(content)
 db = Chroma.from_documents(documents, OpenAIEmbeddings())
 
-# 执行相似性搜索
 query = "What is LLMSingleActionAgent?"
+
+# 执行相似性搜索
 # docs = db.similarity_search(query)
 # print(docs[0].page_content)
 
-# 还可以搜索与给定嵌入向量类似的文档，使用similarity_search_by_vector该向量接受嵌入向量作为参数而不是字符串。
+# 还可以搜索与给定嵌入向量类似的文档，使用 similarity_search_by_vector 该向量接受嵌入向量作为参数而不是字符串。
 embedding_vector = OpenAIEmbeddings().embed_query(query)
 docs = db.similarity_search_by_vector(embedding_vector)
 print(docs[0].page_content)
